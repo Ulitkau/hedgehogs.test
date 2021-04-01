c72a5cb7f71fd229c95b7be3400df5b01f847a22
Фаиг <uliazhil@gmail.com>
2021-04-01 08:22:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ёжики, жизнь-прекрасна!
         </p>
         <a
           className="App-link"
