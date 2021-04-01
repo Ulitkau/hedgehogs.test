a7a7048d0e0bc0d425a6930781ce01c2fdd0c6d3
Фаиг <uliazhil@gmail.com>
2021-04-01 08:36:22 +0300

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Или не всегда()
+          Ёжики, жизнь-прекрасна!
         </p>
         <a
           className="App-link"
